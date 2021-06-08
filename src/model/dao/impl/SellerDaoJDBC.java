d944658a927269ecaa6f77f525f476d8eeedaeb5
Tiago Costa <tiago.alcosta@gmail.com>
2021-06-08 12:34:25 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -82,5 +85,53 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public List<Seller> findAll() {
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId());
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();			
+			Map<Integer, Department> map = new HashMap<>();
+			
+			//isso é feito desta forma para instanciar vários sellers em apenas um department.
+			//enquanto existir uma linha...
+			while (rs.next()) {
+				
+				//a variável dep recebe do map o id do rs(ResultSet)
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				//se não existir o dep ai sim é estanciado
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					//salva no map.
+					map.put(rs.getInt("DepartmentId"), dep);				
+				}
+				
+				//instancia o seller e adiciona tudo na variável obj.
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);		
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);		
+		}
 	}
 }
